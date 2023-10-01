a19cc1995e0958ca09ffc3154b77f2052f95d013
iampz <rhymerealm@gmail.com>
2023-10-01 10:58:45 +0700

    v1.0.0

---
+++ calcDiff.js
@@ -0,0 +1,106 @@
+function splitWords(text) {
+
+  const input = document.createElement('p');
+  const output = [];
+  const sel = window.getSelection();
+
+  input.textContent = text.replace(/ /g, ' !!! ');
+  input.style.cssText = 'position: absolute; top: -100px; ';
+  document.body.appendChild(input);
+  sel.collapse(input, 0);
+  var start = 0;
+  var end = 0;
+
+  while(end < input.textContent.length) {
+    sel.modify('extend', 'forward', 'word');
+    end = sel.focusOffset;
+    output.push( input.textContent.substring(start, end).trim() );
+    start = end;
+  }
+
+  document.body.removeChild(input);
+  return output;
+
+}
+
+function cleanupText(txt) {
+  return txt
+    .replace(/  +/g, ' ')
+    .replace(/^\s+|\s+$/g, '')
+    .split('\n');
+}
+
+function compareWords() {
+
+  const selectedElems = document.querySelectorAll('.selected');
+  if (selectedElems.length === 3) {
+    
+    const lineCompare = document.getElementById('lineCompare');
+    const wordDiff = patienceDiffPlus(
+      splitWords( selectedElems[0].textContent ),
+      splitWords( selectedElems[1].textContent )
+    );
+    
+    var diffWords = '';
+    wordDiff.lines.forEach(o => {
+      if (o.line.trim() === '!!!') {
+        diffWords += ' ';
+      } else if (o.aIndex < 0) {
+        diffWords += `<span class="add">${o.line}</span>`;
+      } else if (o.bIndex < 0) {
+        diffWords += `<span class="remove">${o.line}</span>`;
+      } else {
+        diffWords += `<span>${o.line}</span>`;
+      }
+    });
+    
+    document.getElementById("lineCompare").innerHTML = '<b>Compare words results: </b><br />' + diffWords;
+    
+  } else {
+    document.getElementById("lineCompare").innerHTML = '<b>Select two lines to compare words.</b>';
+  }
+
+}
+
+function calcDiff(evt) {
+  const a = cleanupText( document.getElementById('a').value );
+  const b = cleanupText( document.getElementById('b').value );
+  const diff = patienceDiffPlus(a , b);
+  
+  var diffLines = '';
+  diff.lines.forEach(o => {
+    if (o.bIndex < 0 && o.moved) {
+      diffLines += `<p class="moveout">${o.line}</p>`;
+    } else if (o.moved) {
+      diffLines += `<p class="movein">${o.line}</p>`;
+    } else if (o.aIndex < 0) {
+      diffLines += `<p class="add">${o.line}</p>`;
+    } else if (o.bIndex < 0) {
+      diffLines += `<p class="remove">${o.line}</p>`;
+    } else {
+      diffLines += `<p>${o.line}</p>`;
+    }
+  });
+  diffLines += '<div id="lineCompare" contenteditable><b>Select two lines to compare words.</b></div>';
+  document.getElementById("diff").innerHTML = diffLines;
+  
+  document.querySelectorAll('p.add, p.remove').forEach(
+    elem => {
+      elem.onmouseover = evt => elem.classList.add('over');
+      elem.onmouseout = evt => elem.classList.remove('over');
+      elem.onmouseup = evt => {
+        
+        const elem = evt.target;
+        if (elem.classList.contains('selected')) {
+          elem.classList.remove('selected');
+        } else {
+          elem.classList.add('selected');
+        }
+        
+        compareWords();
+        
+      };
+    }
+  );
+  
+}
